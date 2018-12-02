69fd13c919be9e0dc8b71435f7bf1bf1724ff5ed
Joan Perals <joan.perals@ixds.com>
2018-12-02 23:16:20 +0100

    Show progress bar when processing records in batches

--- src/db.js
+++ src/db.js
@@ -1,30 +1,54 @@
-function doInBatches(callbackFunction, {collection, startFromId, batchSize, limit, doneCount = 0, query = {}}) {
+const cliProgress = require('cli-progress')
+
+let progressBar
+
+function doInBatches(callbackFunction, {collection, startFromId, batchSize, limit, doneCount = 0, query = {}, batchCallBack, message}) {
   return new Promise((resolve, reject) => {
     getRecordBatch({collection, startFromId, batchSize, query})
       .catch(err => {
+        endUpdate()
         reject(err)
       })
       .then(batchItems => {
         if (batchItems.length) {
           doneCount += batchItems.length
           const promises = []
+          if (batchCallBack) {
+            batchCallBack(doneCount)
+          }
+          reportUpdate({doneCount, limit})
           for (const record of batchItems) {
             promises.push(callbackFunction({record, collection}))
           }
           startFromId = batchItems[batchItems.length - 1]._id
           return Promise.all(promises)
         } else {
+          endUpdate()
           resolve(doneCount)
         }
       })
       .then(() => {
         if (doneCount < limit) {
-          resolve(doInBatches(callbackFunction, {collection, startFromId, doneCount, batchSize, limit, query}))
+          resolve(doInBatches(callbackFunction, {
+            collection,
+            startFromId,
+            doneCount,
+            batchSize,
+            limit,
+            query,
+            batchCallBack
+          }))
         } else {
+          endUpdate()
           resolve(doneCount)
         }
       })
   })
+}
+
+function endUpdate() {
+  progressBar.stop()
+  progressBar = undefined
 }
 
 function getRecordBatch({collection, startFromId, batchSize, query = {}}) {
@@ -46,6 +70,14 @@ function getRecordBatch({collection, startFromId, batchSize, query = {}}) {
         _id: 1
       }
     }).toArray()
+}
+
+function reportUpdate({doneCount, limit}) {
+  if (!progressBar) {
+    progressBar = new cliProgress.Bar({}, cliProgress.Presets.shades_classic)
+    progressBar.start(limit, 0)
+  }
+  progressBar.update(doneCount)
 }
 
 module.exports = {
