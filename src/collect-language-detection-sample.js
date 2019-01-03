2a769d1585af032be4ab0663aed141e2f883d4e5
Joan Perals <joan.perals@ixds.com>
2019-01-03 01:14:50 +0100

    Improve language detection test sample

--- src/collect-language-detection-sample.js
+++ src/collect-language-detection-sample.js
@@ -1,29 +1,45 @@
 // Collects a sample of tweets for CLD to test against
 
+const path = require('path')
+const filePath = path.join(__dirname, '..', 'test/language-detection-sample.json')
+
 const {initStream, isTweet} = require('./twitter-stream')
 const jsonfile = require('jsonfile')
-const path = require('path')
+const ProgressBar = require('progress')
+
+let sample
+try {
+  sample = require(filePath)
+} catch(e) {
+  sample = []
+}
 
 const parseArgs = require('minimist')
 
 const limit = Number(parseArgs(process.argv.slice(2)).n) || 100
-
 const stream = initStream()
 
 let nTweets = 0
-const sample = []
+
+const progressBar = new ProgressBar(':bar :percent | :current/:total | ETA: :etas', {
+  complete: '█',
+  incomplete: '░',
+  total: limit
+})
 
 stream.on('data', function(event) {
   if(!isTweet(event)) {
     return
   }
   sample.push({
+    index: nTweets,
     text: event.text,
+    placeName: event.place.full_name,
     lang: ""
   })
   nTweets += 1
+  progressBar.tick()
   if(limit <= nTweets) {
-    const filePath = path.join(__dirname, '..', 'test/cld_sample.json')
     jsonfile.writeFile(filePath, sample, {spaces: 2})
       .then(() => {
         process.exit(0)
