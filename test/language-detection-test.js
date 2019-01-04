e956dc766f2ed9bb7ce67a359d3f28d02939a999
Joan Perals <joan.perals@ixds.com>
2019-01-05 00:25:14 +0100

    Refine language detection test

--- test/language-detection-test.js
+++ test/language-detection-test.js
@@ -16,25 +16,28 @@ describe('The language detection mechanism', () => {
       .then(results => {
         let fails = 0
         let discarded = 0
+        let report = ''
         for (const [index, result] of results.entries()) {
           const expected = sample[index].lang
           const returned = result && result.languages ? result.languages[0].code : ''
-          try {
-            const matches = returned == expected || expected instanceof Array && expected.indexOf(returned) !== -1
-            const tooShort = cleanUp(sample[index].text).length < minimumTweetLength
-            if(tooShort) {
-              discarded += 1
-            }
-            const passes = matches || tooShort
-            assert(passes)
-          } catch(e) {
-            console.warn('\nReturned value is not as expected for text at index ' + index + ':\n' + sample[index].text + '\nExpected: ' + expected + '\nReturned: ' + returned + '\n' + cleanUp(sample[index].text))
+          const matches = returned == expected || expected instanceof Array && expected.indexOf(returned) !== -1
+          const tooShort = cleanUp(sample[index].text).length < minimumTweetLength
+          if (tooShort) {
+            discarded += 1
+          }
+          const passes = matches || tooShort
+          if (!passes) {
+            report += '\nReturned value is not as expected for text at index ' + index + ':\n' + sample[index].text + '\nCleaned up text:\n' + cleanUp(sample[index].text) + '\nExpected: ' + expected + '\nReturned: ' + returned + '\n'
             fails += 1
           }
         }
-        console.log('Discarded:', discarded + '/' + results.length+ ' (' + nDecimals(100*discarded/results.length, 2) + '%)')
+        console.log(report)
+        console.log('Discarded:', discarded + '/' + results.length + ' (' + nDecimals(100 * discarded / results.length, 2) + '%)')
         const translated = results.length - discarded
-        console.log('Bad detections:', fails + '/' + translated + ' (' + nDecimals(100*fails/translated, 2) + '%)')
+        console.log('Bad detections:', fails + '/' + translated + ' (' + nDecimals(100 * fails / translated, 2) + '%)')
+        const discardedOk = discarded / results.length <= 0.65
+        const failsOk = fails / translated <= 0.05
+        assert(discardedOk && failsOk)
         done()
       })
   })
