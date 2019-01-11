eb8b9383c7d8ec0d935dede30457c6547001fa50
Joan Perals <joan.perals@ixds.com>
2019-01-12 00:11:03 +0100

    Refactor: standalone `languageName` function

---
+++ src/language-name.js
@@ -0,0 +1,30 @@
+// Returns the nice language name that corresponds to a language code
+
+const languageNameOverrides = require('./language-name-overrides')
+
+const cld = require('cld')
+const languagesByCode = {}
+
+for(const cldName in cld.LANGUAGES) {
+  const code = cld.LANGUAGES[cldName]
+  const override = languageNameOverrides[code]
+  languagesByCode[code] = override ? override : capitalize(cldName)
+}
+
+function languageName(languageCode) {
+  return languagesByCode[languageCode]
+}
+
+// Transform only first letter of a string to uppercase and the rest to lowercase
+// Based on
+// https://github.com/grncdr/js-capitalize/blob/master/index.js
+function capitalize(str) {
+  try {
+    str = str.toLowerCase()
+    return str.charAt(0).toUpperCase() + str.substring(1)
+  } catch (error) {
+    return str
+  }
+}
+
+module.exports = languageName
--- src/legend.js
+++ src/legend.js
@@ -7,7 +7,7 @@ const cld = require('cld')
 const {finish, handleRejection} = require('./common.js')
 const jsonfile = require('jsonfile')
 const languageColor = require('./language-color')
-const languageNames = require('./language-names')
+const languageName = require('./language-name')
 const parseArgs = require('minimist')
 
 getLanguages()
@@ -19,9 +19,9 @@ function getLanguages() {
   return new Promise((resolve, reject) => {
     const languages = {}
     // Reverse key-value relationship
-    for (const languageName in cld.LANGUAGES) {
-      const languageCode = cld.LANGUAGES[languageName]
-      const prettyLanguageName = languageNames[languageCode] ? languageNames[languageCode] : capitalize(languageName)
+    for (const name in cld.LANGUAGES) {
+      const languageCode = cld.LANGUAGES[name]
+      const prettyLanguageName = languageName(languageCode)
       languages[languageCode] = {
         code: languageCode,
         color: languageColor(languageCode),
@@ -44,17 +44,5 @@ function printLegend(languageData) {
       const colorize = chalk.hex(color)
       console.log(colorize('███'), name, '(' + code + '):', colorize(color))
     }
-  }
-}
-
-// Transform only first letter of a string to uppercase and the rest to lowercase
-// Based on
-// https://github.com/grncdr/js-capitalize/blob/master/index.js
-function capitalize(str) {
-  try {
-    str = str.toLowerCase()
-    return str.charAt(0).toUpperCase() + str.substring(1)
-  } catch (error) {
-    return str
   }
 }
